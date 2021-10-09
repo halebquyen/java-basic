1ee0a8e941cb2c1a86817c2691d2f977194ca394
halebquyen <halebquyen@gmail.com>
2021-10-09 10:51:17 +0700

    add Exercises2 of Bài 10: Exercises Array & String

--- Java/src/test/java/day5/Exercises_Day5.java
+++ Java/src/test/java/day5/Exercises_Day5.java
@@ -5,23 +5,17 @@ import java.util.Scanner;
 public class Exercises_Day5 {
     public static void main(String args[]) {
         //int[] array = enterElements();
-        int [] array1 = {10, -20, 0, 30, 40, 60, 10};
-        int [] array2 = {10, -20, 0, 30, 40, 60, 2};
-        int [] array3 = {2, -20, 0, 30, 40, 60, 10};
-        int [] array4 = {10};
-        int [] array5 = {10, 10};
-        int [] array6 = {10, 10, 10};
-        int [] array7 = {10, 10, 20};
-        int [] array8 = {2, 10, 10};
+        int [] array1 = {50, -20, 0, 30, 40, 60, 10};
+        int [] array2 = {50, -20, 0, 30, 40, 60, 50};
+        int [] array3 = {10};
+        int [] array4 = {10, 10};
+        int [] array5 = {10, 10, 10};
 
-        print("array1:true " + validArrayElementFirstLast(array1));
-        print("array2:true " + validArrayElementFirstLast(array2));
-        print("array3:true " + validArrayElementFirstLast(array3));
-        print("array4:false " + validArrayElementFirstLast(array4));
-        print("array5:false " + validArrayElementFirstLast(array5));
-        print("array6:true " + validArrayElementFirstLast(array6));
-        print("array7:true " + validArrayElementFirstLast(array7));
-        print("array8:true " + validArrayElementFirstLast(array7));
+        print("array1:false " + validArrayElementFirstAndLastTheSame(array1));
+        print("array2:true " + validArrayElementFirstAndLastTheSame(array2));
+        print("array3:false " + validArrayElementFirstAndLastTheSame(array3));
+        print("array4:false " + validArrayElementFirstAndLastTheSame(array4));
+        print("array5:true " + validArrayElementFirstAndLastTheSame(array5));
     }
 
     /*
@@ -29,16 +23,34 @@ public class Exercises_Day5 {
     Viết chương trình kiểm tra nếu số 10 xuất hiện ở đầu hoặc cuối trong mảng. Độ dài của mảng phải lớn hơn 2.
     Sample Output:
     Test Data: array = 10, -20, 0, 30, 40, 60, 10
+    true
      */
     public static boolean validArrayElementFirstLast(int[] array) {
         boolean result = false;
         int lengthArray = array.length;
-        if ( lengthArray > 2) {
-            if (array[0] == 10 || array[lengthArray-1] == 10) {
+        if (lengthArray > 2) {
+            if (array[0] == 10 || array[lengthArray - 1] == 10) {
                 result = true;
             }
+        } else {
+            result = false;
         }
-        else {
+        return result;
+    }
+
+    /*2. Write a Java program to test if the first and the last element of an array of integers are the same. The length of the array must be greater than or equal to 2.
+    Viết chương trình kiểm tra phần tử đầu và phần tử trong mảng giống nhau. Độ dài của mảng phải lớn hơn 2.
+    Test Data: array = 50, -20, 0, 30, 40, 60, 10
+    Sample Output: false
+    */
+    public static boolean validArrayElementFirstAndLastTheSame(int[] array) {
+        boolean result = false;
+        int lengthArray = array.length;
+        if (lengthArray > 2) {
+            if (array[0] == array[lengthArray - 1]) {
+                result = true;
+            }
+        } else {
             result = false;
         }
         return result;
@@ -47,12 +59,15 @@ public class Exercises_Day5 {
     public static void print(int value) {
         System.out.println(value);
     }
+
     public static void print(boolean value) {
         System.out.println(value);
     }
+
     public static void print(String value) {
         System.out.println(value);
     }
+
     public static int enterNumber() {
         System.out.print("Enter Number: ");
         int n = 0;
